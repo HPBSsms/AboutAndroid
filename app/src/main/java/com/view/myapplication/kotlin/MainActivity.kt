b7cb2bc29c386d14a5b88685a196952e08459091
Smith <1379532449@qq.com>
2021-12-01 17:08:42 +0800

    kotlin

--- app/src/main/java/com/view/myapplication/kotlin/MainActivity.kt
+++ app/src/main/java/com/view/myapplication/kotlin/MainActivity.kt
@@ -2,12 +2,30 @@ package com.view.myapplication.kotlin
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.view.myapplication.R
+import com.view.myapplication.databinding.ActivityMainKtBinding
+import com.view.myapplication.kotlin.data.User
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-        setContentView(R.layout.activity_main)
+    private val activityMainKtBinding: ActivityMainKtBinding
+        get() {
+            val binding: ActivityMainKtBinding =
+                DataBindingUtil.setContentView(this, R.layout.activity_main_kt)
+            return binding
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding: ActivityMainKtBinding =
+            activityMainKtBinding
+        binding.user = User("Smith", "Kuc")
+    }
+
+    fun bt1(view: View) {
+        Toast.makeText(this, "点击按钮", Toast.LENGTH_SHORT).show()
     }
 }
---
+++ app/src/main/java/com/view/myapplication/kotlin/data/User.kt
@@ -0,0 +1,3 @@
+package com.view.myapplication.kotlin.data
+
+data class User(val firstName: String, val lastName: String)
